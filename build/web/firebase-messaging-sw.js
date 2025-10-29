eadabd216b37b37ab941aa05c8eae5bd043eeea7
community-vercel <saborly02@gmail.com>
2025-10-29 16:53:34 +0500

    Add Flutter web build

---
+++ build/web/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+   importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyB8lm-4t0Ox8OkZODqLgnj_AKYfd8iyIGI",
+  authDomain: "saborly.firebaseapp.com",
+  projectId: "saborly",
+  storageBucket: "saborly.firebasestorage.app",
+  messagingSenderId: "361344460853",
+  appId: "1:361344460853:web:c680024379e3a4c57c697c",
+  measurementId: "G-ZCWJE1NKQB"
+};
+   const messaging = firebase.messaging();
